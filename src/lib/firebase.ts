7b644f6608a52bfa478fcb4aa2226a4c9718f7f0
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-08-23 17:05:22 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyD96j5pBO3Cyrq5xAyUiOtuRA1Mc2KK7nE",
+  authDomain: "mitraai-x30cv.firebaseapp.com",
+  projectId: "mitraai-x30cv",
+  storageBucket: "mitraai-x30cv.firebasestorage.app",
+  messagingSenderId: "553364420519",
+  appId: "1:553364420519:web:a9352d44e7d5b4dda16882"
 };
 
 // Initialize Firebase
