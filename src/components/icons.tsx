b0a9c4c6946144d28f877d32f9ce6b1efa9f0a88
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-08-24 21:28:12 +0000

    can you change mitra ai logo, i want a robot with heart

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,9 +12,16 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-      <path d="M7.5 3C9 3 10 4 11 5" />
-      <path d="M16.5 3c-1.5 0-2.5 1-3.5 2" />
+      <path d="M12 8V4H8" />
+      <rect width="16" height="12" x="4" y="8" rx="2" />
+      <path d="M2 14h2" />
+      <path d="M20 14h2" />
+      <path d="M15 13v2" />
+      <path d="M9 13v2" />
+      <path d="m12.25 12.75-.5-1.5-.5 1.5" />
+      <path d="M12.25 15.75a1 1 0 0 0-1.5 0" />
+      <path d="M8.5 12.5a1 1 0 0 0-1 1" />
+      <path d="M15.5 12.5a1 1 0 0 1 1 1" />
     </svg>
   );
 }
