b8dca572099b8fedc5c2b837ba6949b02ff274c0
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-09-05 22:47:36 +0000

    create logo like this

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,15 +7,14 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="2.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
-      <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
-      <path d="M8 12h8" />
-      <path d="M9 16h0" />
-      <path d="M15 16h0" />
+      <path d="M4 18.5V9.5C4 7 5 5 8 5c3 0 4 2 4 4.5v9" />
+      <path d="M12 18.5V9.5c0-2.5 1-4.5 4-4.5s4 2 4 4.5v9" />
+      <path d="M10 12h4" />
     </svg>
   );
 }
