65f31a9e7259a30742d033d9f5d8056d80e90c20
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-09-05 22:38:20 +0000

    its exceelent but the logo is not good all the letters are good, can you

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -18,10 +18,6 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       <path d="M20 14h2" />
       <path d="M15 13v2" />
       <path d="M9 13v2" />
-      <path d="m12.25 12.75-.5-1.5-.5 1.5" />
-      <path d="M12.25 15.75a1 1 0 0 0-1.5 0" />
-      <path d="M8.5 12.5a1 1 0 0 0-1 1" />
-      <path d="M15.5 12.5a1 1 0 0 1 1 1" />
     </svg>
   );
 }
