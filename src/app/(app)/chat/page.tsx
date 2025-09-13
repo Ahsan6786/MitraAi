e52735c908a4d3537064a00b472ea681707e3e42
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-09-13 19:04:39 +0000

    please fix, it is not fixing

--- src/app/(app)/chat/page.tsx
+++
@@ -1,6 +0,0 @@
-
-// This file is now replaced by the dynamic route at /chat/[[...conversationId]]/page.tsx
-// To keep Next.js happy, we can just re-export the new page, but it's better to delete this file
-// and rely on the dynamic route to handle both /chat and /chat/[id]. For this tool, we'll replace it.
-
-export { default } from './[[...conversationId]]/page';
