9c9ceaa9b3192545d59e614b21a49e4bbbadf12b
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-09-13 20:34:14 +0000

    remove the tawk.to integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,39 +33,6 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
-        
-        {/* <!--Start of Tawk.to Script--> */}
-        <Script id="tawk-to-script" strategy="lazyOnload">
-          {`
-            var Tawk_API = Tawk_API || {};
-            Tawk_API.onLoad = function(){
-                Tawk_API.customStyle = {
-                    visibility : {
-                        desktop : {
-                            xOffset : '20px',
-                            yOffset : '80px',
-                            position : 'br'
-                        },
-                        mobile : {
-                            xOffset : '10px',
-                            yOffset : '80px',
-                            position : 'br'
-                        }
-                    }
-                };
-            };
-            var Tawk_LoadStart = new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/68c4809b67c586192c677ae3/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
-        {/* <!--End of Tawk.to Script--> */}
       </body>
     </html>
   );
